9db535ce704a020b967c7bd534b757c0f944ec3c
leesunnyy <165560447+leesunnyy@users.noreply.github.com>
2024-04-17 17:03:44 +0900

    commit

--- xml_to_csv.py
+++ xml_to_csv.py
@@ -8,6 +8,9 @@ python xml_to_csv.py -i [PATH_TO_IMAGES_FOLDER]/test -o [PATH_TO_ANNOTATIONS_FOL
 """
 #modifed by Prof. Kim 2024. 03. 22
 
+# edit by leesunnyy
+
+
 import os
 import glob
 import pandas as pd
